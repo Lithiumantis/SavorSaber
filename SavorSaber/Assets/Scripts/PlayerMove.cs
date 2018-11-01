b4bc254e7c8e04b596e71e01bb4b2982c2378b5c
Lithiumantis <juyyek@ucsc.edu>
2018-10-31 23:47:37 -0700

    Basic player object

---
+++ SavorSaber/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    //public variables
+    public float speed = 0.5f;
+
+    //private variables
+    private float xInputValue;
+    private float yInputValue;
+    private Rigidbody2D rigidbody;
+
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Start ()
+    {
+
+	}
+	
+
+	void Update ()
+    {
+        xInputValue = Input.GetAxis("Horizontal");
+        yInputValue = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 movement = new Vector2(xInputValue * speed, yInputValue * speed);
+        rigidbody.MovePosition(rigidbody.position + movement);
+    }
+}
