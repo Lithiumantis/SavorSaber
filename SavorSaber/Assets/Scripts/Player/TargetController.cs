781f29af5ec59e0440e7bb729d30d174915e539d
Lithiumantis <juyyek@ucsc.edu>
2018-11-03 14:28:25 -0700

    Tilted targeting circle to better match camera "perspective"

--- SavorSaber/Assets/Scripts/Player/TargetController.cs
+++ SavorSaber/Assets/Scripts/Player/TargetController.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class TargetController : MonoBehaviour {
 
     //Quaternion for 
-    private Quaternion tempRotation;
+    private Quaternion mouseRotation;
+    private Quaternion startRotation;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        startRotation = transform.rotation;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        //test code to make the target rotator move in perspective (i.e., in 3D space)
-        //tempRotation = Quaternion.LookRotation(Vector3.forward, mousepos - transform.position);
-        //transform.localRotation = new Quaternion(transform.rotation.x, tempRotation.z, transform.rotation.z, transform.rotation.w);
-        transform.rotation = Quaternion.LookRotation(Vector3.forward, mousepos - transform.position);
+        //get the angle between the mouse cursor and player, extract z rotation by translating to Euler
+        Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseRotation = Quaternion.LookRotation(Vector3.forward, mousepos - transform.position);
+        Vector3 eulerMouseRotation = mouseRotation.eulerAngles;
+        float eulerMouseZ = eulerMouseRotation.z;
+
+        //apply z rotation of mouse angle to circle's z rotation by translating back from Euler
+        Vector3 eulerCircleRotation = transform.rotation.eulerAngles;
+        eulerCircleRotation.z = eulerMouseZ;
+        Quaternion newRotation = Quaternion.Euler(eulerCircleRotation);
+        transform.rotation = newRotation;
+
+
     }
 }
