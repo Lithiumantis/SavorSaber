283cc9cfd629c347d9f7004b2b132b7134c04e50
Justin Pimentel <jfpiment@ucsc.edu>
2018-11-05 19:48:20 -0800

    Monster movement
    Added 2 scripts that let monsters run and chase a given target. Speed and range are also configurable.

---
+++ SavorSaber/Assets/Scripts/Monsters/Run.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run : MonoBehaviour {
+
+	public Transform Player;
+	public int moveSpeed = 3;
+	public int minDist = 5;
+	private float range;
+
+	// Update is called once per frame
+	void Update () {
+		range = Vector2.Distance (transform.position, Player.position);
+
+		if(range < minDist){ 
+			transform.position = Vector2.MoveTowards (transform.position, Player.transform.position, (-1) * moveSpeed * Time.deltaTime);
+		}
+
+	}
+}
