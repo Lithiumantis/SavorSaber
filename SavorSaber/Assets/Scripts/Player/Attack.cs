b80a4deeac763522794648bea4d66f17d3dba083
Lithiumantis <juyyek@ucsc.edu>
2018-11-08 18:59:29 -0800

    Fixed UI bugs, merged monster attacking into development

--- SavorSaber/Assets/Scripts/Player/Attack.cs
+++ SavorSaber/Assets/Scripts/Player/Attack.cs
@@ -20,6 +20,9 @@ public class Attack : MonoBehaviour {
     private SpriteRenderer slashRenderer;
     private SpriteRenderer spearRenderer;
 
+    private CircleCollider2D slashCollider;
+    private CapsuleCollider2D spearCollider;
+
     private float leftMouseInputValue;
     private float rightMouseInputValue;
 
@@ -42,6 +45,8 @@ public class Attack : MonoBehaviour {
         spearRenderer = spear.GetComponent<SpriteRenderer>();
         slashRenderer = slashVisual.GetComponent<SpriteRenderer>();
         targetController = GetComponent<TargetController>();
+        slashCollider = GetComponent<CircleCollider2D>();
+        spearCollider = knife.GetComponent<CapsuleCollider2D>();
     }
 	
 	void FixedUpdate ()
@@ -152,6 +157,7 @@ public class Attack : MonoBehaviour {
             knifeRenderer.enabled = false;
             spearRenderer.enabled = false;
             slashRenderer.enabled = true;
+            slashCollider.enabled = true;
 
             slashVisual.transform.RotateAround(slashVisual.transform.position, -1 * slashVisual.transform.forward, Time.deltaTime * rotateSpeed);
 
@@ -172,10 +178,22 @@ public class Attack : MonoBehaviour {
         knifeRenderer.enabled = true;
         spearRenderer.enabled = true;
         slashRenderer.enabled = false;
+        slashCollider.enabled = false;
 
         yield return null;
     }
 
-// FUNCTIONS TO ACTUALLY INTERACT WITH ENEMIES
+    // FUNCTIONS TO ACTUALLY INTERACT WITH ENEMIES
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("touching");
+
+        if (collision.gameObject.tag == "Monster")
+        {
+            Debug.Log("MONSTER SLASHED");
+            collision.gameObject.active = false;
+        }
+    }
 
 }
