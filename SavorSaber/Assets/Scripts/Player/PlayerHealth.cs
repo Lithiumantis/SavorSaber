e9516c5d71a2b7a73d1b4425a4099bdafa667eda
Lithiumantis <juyyek@ucsc.edu>
2018-11-13 19:51:37 -0800

    Basic player health script

---
+++ SavorSaber/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int maxHealth = 10;
+    private int health;
+
+
+	// Use this for initialization
+	void Start () {
+        health = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TakeDamage(int dmg)
+    {
+        health--;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void ResetHealth()
+    {
+        health = maxHealth;
+    }
+
+    public void Die()
+    {
+        Debug.Log("Player dead");
+
+        //whatever we want to happen on death. Animations, etc. Respawn? 
+    }
+}
