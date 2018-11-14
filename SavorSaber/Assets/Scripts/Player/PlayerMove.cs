f836e5eb09fe09ca968519584aaa8fe58a727774
Tina Peng <tpeng3@ucsc.edu>
2018-11-13 19:11:09 -0800

    added player walking anim

--- SavorSaber/Assets/Scripts/Player/PlayerMove.cs
+++ SavorSaber/Assets/Scripts/Player/PlayerMove.cs
@@ -11,6 +11,7 @@ public class PlayerMove : MonoBehaviour {
     private float xInputValue;
     private float yInputValue;
     private Rigidbody2D rigidbody;
+    private Animator anim;
 
 // MONOBEHAVIOR FUNCTIONS -------------------------------------------------------------------------
 
@@ -19,16 +20,46 @@ public class PlayerMove : MonoBehaviour {
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
 	void Update ()
     {
         //get horiz and vert input
         xInputValue = Input.GetAxisRaw("Horizontal");
         yInputValue = Input.GetAxisRaw("Vertical");
+        anim.SetFloat("SpeedX", xInputValue);
+        anim.SetFloat("SpeedY", yInputValue);
+
     }
 
     private void FixedUpdate()
     {
         Move();
+        float xLastInput = Input.GetAxisRaw("Horizontal");
+        float yLastInput = Input.GetAxisRaw("Vertical");
+        if(xLastInput != 0 || yLastInput != 0){
+            anim.SetBool("Walking", true);
+            if(xLastInput > 0){
+                anim.SetFloat("LastMoveX", 1f);
+            }else if(xLastInput < 0){
+                anim.SetFloat("LastMoveX", -1f);
+            }else{
+                anim.SetFloat("LastMoveX", 0f);
+            }
+
+            if(yLastInput > 0){
+                anim.SetFloat("LastMoveY", 1f);
+            }else if(yLastInput < 0){
+                anim.SetFloat("LastMoveY", -1f);
+            }else{
+                anim.SetFloat("LastMoveY", 0f);
+            }
+        }else{
+            anim.SetBool("Walking", false);
+        }
     }
 
     private void Move()
