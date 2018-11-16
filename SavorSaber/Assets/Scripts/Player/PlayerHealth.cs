5f9df7f0b6b25437bf147ae63a1b6e206d49c87b
Lithiumantis <juyyek@ucsc.edu>
2018-11-16 01:10:57 -0800

    Health bar added
    Just a test, not part of the actual UI

--- SavorSaber/Assets/Scripts/Player/PlayerHealth.cs
+++ SavorSaber/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 
     public int maxHealth = 10;
     public int health;
-
+    public Slider slider;
+    public Image fillImage;
 
 	// Use this for initialization
 	void Start () {
@@ -14,13 +16,19 @@ public class PlayerHealth : MonoBehaviour {
 
         //use this to test healing mechanics 
         health = 5;
-	}
+
+        UpdateUI();
+    }
 
     public void TakeDamage(int dmg)
     {
         health -= dmg;
+        UpdateUI();
+
         Debug.Log("Player took " + dmg + " damage!");
         Debug.Log("Player Health: " + health);
+
+
         if(health <= 0)
         {
             Die();
@@ -30,6 +38,7 @@ public class PlayerHealth : MonoBehaviour {
     public void ResetHealth()
     {
         health = maxHealth;
+        UpdateUI();
     }
 
     public void Die()
@@ -42,12 +51,19 @@ public class PlayerHealth : MonoBehaviour {
     public void Heal(int addedHealth)
     {
         health += addedHealth;
-        
+        UpdateUI();
+
         if (health > maxHealth)
         {
             health = maxHealth;
         }
 
         Debug.Log("healed to " + health);
+    }
+
+    private void UpdateUI()
+    {
+        Debug.Log("Updating health bar. Health = " + health);
+        slider.value = health;
     }
 }
