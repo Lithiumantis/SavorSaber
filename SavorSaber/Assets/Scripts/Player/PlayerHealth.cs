d79b11bb7d268089a5ba80e51080689216700315
Lithiumantis <juyyek@ucsc.edu>
2018-11-16 02:14:57 -0800

    Camera changes

--- SavorSaber/Assets/Scripts/Player/PlayerHealth.cs
+++ SavorSaber/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,14 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 
     public int maxHealth = 10;
     public int health;
-    public Slider slider;
-    public Image fillImage;
+
 
 	// Use this for initialization
 	void Start () {
@@ -16,19 +14,13 @@ public class PlayerHealth : MonoBehaviour {
 
         //use this to test healing mechanics 
         health = 5;
-
-        UpdateUI();
-    }
+	}
 
     public void TakeDamage(int dmg)
     {
         health -= dmg;
-        UpdateUI();
-
         Debug.Log("Player took " + dmg + " damage!");
         Debug.Log("Player Health: " + health);
-
-
         if(health <= 0)
         {
             Die();
@@ -38,7 +30,6 @@ public class PlayerHealth : MonoBehaviour {
     public void ResetHealth()
     {
         health = maxHealth;
-        UpdateUI();
     }
 
     public void Die()
@@ -51,19 +42,12 @@ public class PlayerHealth : MonoBehaviour {
     public void Heal(int addedHealth)
     {
         health += addedHealth;
-        UpdateUI();
-
+        
         if (health > maxHealth)
         {
             health = maxHealth;
         }
 
         Debug.Log("healed to " + health);
-    }
-
-    private void UpdateUI()
-    {
-        Debug.Log("Updating health bar. Health = " + health);
-        slider.value = health;
     }
 }
